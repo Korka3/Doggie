b0d9f59ab0f4a8308c52c2060a54313ce0891008
Korka3 <93039459+Korka3@users.noreply.github.com>
2022-09-07 13:10:27 +0200

    drop menu uslugi

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// $(function() {
+//     $("c-nav__unfold-menu").on('click', function() {
+//       $(".c-dropdown").toggleClass("c-dropdown--active");
+//     //   $(".c-nav__mobile").toggleClass("c-nav__mobile--ontop");
+//     //   $(".c-hamburger--active").toggleClass(".c-hamburger");
+
+//     });
+//   });
+
+// $(function(){
+//       $("c-dropdown").hide()});
+
+$(function() {
+     $(".c-nav__show-menu").on('click', function() {
+        $(".c-wraper").toggleClass("c-wraper--active")
+    });
+});
